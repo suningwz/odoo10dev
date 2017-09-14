5674ba551934fa482c10ef05c19aeea6b7e990ba
suningwz <2437526685@qq.com>
2017-09-14 11:34:34 +0800

    base.archive

--- custom-addons/my_module/models/library_book.py
+++ custom-addons/my_module/models/library_book.py
@@ -9,6 +9,20 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.addons import decimal_precision as dp
 
+
+class BaseArchive(models.AbstractModel):
+    _name='base.archive'
+    active = fields.Boolean(
+        string=u'Active?',
+        default=True
+    )
+
+    def do_archive(self):
+        for record in self:
+            record.active=not record.active
+
+            
+
 class LibraryBook(models.Model):
     """ The summary line for a class docstring should fit on one line.
 
@@ -18,10 +32,14 @@ class LibraryBook(models.Model):
     """
 
     _name = 'library.book'
+    
+    _inherit = ['base.archive']
+    
     _description = u'library book'
 
     _rec_name = 'short_name'
     _order = 'date_release desc,name'
+    _sql_constraints = [('name_uniq','UNIQUE(name)','Book title must be unique.')]
 
     name = fields.Char(
         string=u'Title',
@@ -73,8 +91,8 @@ class LibraryBook(models.Model):
         comodel_name='res.currency',
     )
     retail_price=fields.Monetary(
-        string=u'Retail Price'
-        #currency_field='currency_id'
+        string=u'Retail Price',
+        currency_field='currency_id'
         )
     
 
@@ -88,6 +106,71 @@ class LibraryBook(models.Model):
         ondelete='set null',
     )
 
+    
+    @api.constrains('date_release')
+    def _check_date_release(self):
+        for record in self:
+            if record.date_release>fields.Date.today():
+                raise models.ValidationError('Release date must be in the past.')
+
+    
+    age_days = fields.Float(
+        string=u'Days since release',       
+        compute='_compute_age',
+        inverse='_inverse_age',
+        search='_search_age'
+    )
+
+    @api.depends('date_release')
+    def _compute_age(self):
+        today=fields.Date.from_string(fields.Date.today())
+        for record in self.filtered('date_release'):
+            delta = fields.Date.from_string(record.date_release)-today
+            self.age_days=delta.days    
+    
+
+    def _inverse_age(self):
+        today=fields.Date.from_string(fields.Date.today())
+        for book in self:
+            book_release=today-datetime.timedelta(days=book.age_days)
+            book.date_release=fields.Date.to_string(book_release)
+
+    def _search_age(self,operator,value):
+        today=fields.Date.from_string(fields.Date.today())
+        value_days=datetime.timedelta(days=value)
+        release_date=today-value_days
+        value_date=fields.Date.to_string(release_date)
+        return [('date_release',operator,value_date)]
+
+   
+    publisher_city = fields.Char(
+        string=u'Publisher City',
+        related='publisher_id.city',
+    )
+    state = fields.Selection(
+        string=u'State',
+        selection=[('draft', 'Unvailable'), ('available', 'Available'),('borrowed','Borrowed'),('lost','Lost')]
+    )
+  
+    @api.model
+    def is_allowed_transition(self,old_state,new_state):
+        allowed = [('draft','unvailable'),('unvailable','vailable'),('vailable','borrowed'),('borrowed','available'),
+        ('available','lost'),('borrowed','lost'),('lost','available')]
+        return (old_state,new_state) in allowed
+
+    @api.multi
+    def change_state(self,new_state):
+        for book in self:
+            if book.is_allowed_transition(book.state,new_state):
+                book.state=new_state
+            else:
+                continue
+    
+
+
+    
+    
+    
 
 
 
@@ -121,7 +204,7 @@ class BookCategory(models.Model):
         comodel_name='library.book.category',
         ondelete='restrict',
     )
-    child_id = fields.One2many(
+    child_ids = fields.One2many(
         string=u'Child Categories',
         comodel_name='library.book.category',
         inverse_name='parent_id',
@@ -138,8 +221,27 @@ class BookCategory(models.Model):
             
     
 
-   
-        
+class LibraryMember(models.Model):
+    _name='library.member'
+    _inherits = {'res.partner': 'partner_id'}
+    
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        ondelete='cascade',
+    )
+    date_start = fields.Date(
+        string=u'Member Since',
+        default=fields.Date.context_today,
+    )
+    date_end = fields.Date(
+        string=u'Termination Date',
+        default=fields.Date.context_today,
+    )
+    member_number = fields.Char()
+    
+    
+    
+    
     
     
 
